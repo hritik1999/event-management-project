a3e2596cd47adab685dc4bdbd5f2561650379c9f
hritik1999 <65231550+hritik1999@users.noreply.github.com>
2025-12-10 18:27:23 +0530

    removed node file"

--- fullstack-app/server/dist/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const AdminController_1 = require("../controllers/AdminController");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.get("/stats", auth_middleware_1.authenticate, (0, auth_middleware_1.authorize)(["ADMIN"]), AdminController_1.AdminController.getStats);
-exports.default = router;
